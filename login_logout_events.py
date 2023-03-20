4aad350d8f65fd683a2d41627198a158d342d8b1
Saint7Sinner6 <70413692+Saint7Sinner6@users.noreply.github.com>
2023-03-19 17:40:43 -0700

    Added login_logout_events.py IT project

---
+++ login_logout_events.py
@@ -0,0 +1,31 @@
+def get_event_date(event):
+    return event.date
+
+def current_users(events):
+    events.sort(key=get_event_date)
+    machines = {}
+    for event in events:
+        if event.machine not in machines:
+            machines[event.machine] = set()
+        if event.type = "login":
+            machines[event.machine].add(event.user)
+        elif event.type = "logout":
+            machines[event.machine].remove(event.user)
+    return machines
+
+def generate_report(machines):
+    for machine, users in machines.items():
+        if len(users) > 0:
+            user_list = ", ".join(users)
+            print("{}: {}".format(machine,user_list))
+
+class Event:
+    def __init__(self, event_date, event_type, machine_name, user):
+        self.date = event_date
+        self.type = event_type
+        self.machine = machine_name
+        self.user = user
+
+events = [
+    Event('2020-01-')
+]
